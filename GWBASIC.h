e83016cf2cc1ad74a9fc23274022d9cfc3fb2067
杨铸人 <49382681@qq.com>
2025-09-25 02:19:04 +0800

    update

---
+++ GWBASIC.h
@@ -0,0 +1,41 @@
+#ifndef __GWBASIC_H__
+#define __GWBASIC_H__
+#include "ptoc.h"
+#include "io.h"
+#include "array.h"
+#include "set.h"
+#include "graph.h"
+#include "paslib.h"
+
+#define low(x) (0)
+#define high(x) (x.size()-1)
+#define C40 1
+#define C80 3
+#define blockread(f, buf, size, read) 
+#define format(a,b,c) ("")
+
+size_t length(string& s);
+
+void flush();
+byte wherey();
+byte wherex();
+void window(byte x,byte y,byte w,byte h);
+void gotoxy(byte x,byte y);
+void clreol();
+void textcolor(byte color);
+void textbackground(byte color);
+void clrscr();
+void getdir(int i, string& s);
+float frac(float v);
+void sound(int f);
+void nosound();
+void chdir(string& path);
+void mkdir(string& path);
+void rmdir(string& path);
+void erase(untyped_file& f);
+void textmode(byte mode);
+int doserror();
+
+int reset(untyped_file& f, int i);
+integer round(real v);
+#endif
---
+++ UTILS.cxx
@@ -0,0 +1,71 @@
+#include "GWBASIC.h"
+
+text output;
+text input;
+
+integer round(real x)
+{
+	return x >= 0 ? trunc(x + 0.5) : trunc(x - 0.5);
+}
+size_t length(string& s) {
+	return s.length();	
+}
+void flush() {
+
+}
+byte wherey() {
+	return 0;
+}
+byte wherex() {
+	return 0;
+}
+void window(byte x,byte y,byte w,byte h) {
+}
+void gotoxy(byte x,byte y) {
+}
+void clreol() {
+}
+void textcolor(byte color) {
+}
+void textbackground(byte color) {
+}
+void clrscr() {
+}
+void getdir(int i, string& s) {
+	
+}
+float frac(float v) {
+	return v - (integer)v;
+}
+
+void sound(int f) {
+}
+void nosound() {
+
+}
+void chdir(string& path) {
+
+}
+void mkdir(string& path) {
+
+}
+void rmdir(string& path) {
+
+}
+
+void erase(untyped_file& f) {
+
+}
+
+void textmode(byte mode) {
+
+}
+
+int doserror() {
+	return 0;
+}
+
+int reset(untyped_file & f, int i) {
+	return 0;
+}
+
